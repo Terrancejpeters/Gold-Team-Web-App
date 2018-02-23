452800a0295239eab33ff4131395dd255b9af8a8
Ethan <etjohn2345@hotmail.com>
2018-02-22 19:59:21 -0500

    Attempted a php
    tried to get a .php file to work, leaving it instead of the header html

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+echo "<link rel='shortcut icon' href='media/logo_splash.png' />";
+echo "<link href='general_style.css' type='text/css' rel='stylesheet'>";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>";
+echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>";
+echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
+echo "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>";
+?>

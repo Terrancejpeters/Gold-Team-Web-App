2c162593aa1146f655a3b0a963718115b6f35a99
Ethan <etjohn2345@hotmail.com>
2018-03-29 17:46:01 -0400

    Working feed and showing login

--- gold_team/archive/urls.py
+++ gold_team/archive/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.feed, name='feed'),
+    path('', views.PostListView.as_view(), name='feed'),
     #path('home/', views.PostListView.as_view(), name='post'),
     #path('home/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
     path('login/', views.login, name='login'),

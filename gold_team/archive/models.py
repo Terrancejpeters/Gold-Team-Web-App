fa8db5f14942ca892f69fcab9877105ea0a84edc
nslabczynski <nslabczynski@gmail.com>
2018-03-29 16:08:57 -0400

    Fixed user model

--- gold_team/archive/models.py
+++ gold_team/archive/models.py
@@ -89,7 +89,7 @@ class User(models.Model):
         """
         String for representing the Model object.
         """
-        return '{0}'.format(self.first_name)
+        return '{0}'.format(self.username)
 
 class Topic(models.Model):
     """

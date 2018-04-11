76dc353c20bea47930008e3ff0b9b680b68393ae
hunnabreen <35741316+hunnabreen@users.noreply.github.com>
2018-04-11 17:21:05 -0400

    old discrepencies

--- gold_team/archive/urls.py
+++ gold_team/archive/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='feed'),
-    #path('home/', views.PostListView.as_view(), name='post'),
-    #path('home/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('', views.feed, name='feed'),
+    path('feed/', views.PostListView.as_view(), name='post'),
+    path('feed/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
     path('login/', views.login, name='login'),
     #path('profile/login/', views.login, name='login'),
 ]

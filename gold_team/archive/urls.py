ea795f2df712e852d3f07ed416adbc840eb7ba10
nslabczynski <nslabczynski@gmail.com>
2018-04-30 18:34:24 -0400

    Fixed Redundancies

--- gold_team/archive/urls.py
+++ gold_team/archive/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='feed'),
     #path('post/<int:pk>', views.PostListView.as_view(), name='post'),
     #path('home/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
-    path('', views.feed, name='feed'),
+  
     path('feed/', views.PostListView.as_view(), name='post'),
     #path('feed/<uuid:pk>', views.PostDetailView.as_view(), name='post-detail'),
     path('login/', views.login, name='login'),
